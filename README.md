0b669253ffd0c6a6c62648521eab89c011d4f11a
Kanishka sah <kanishkasah2003@gmail.com>
2024-03-12 00:26:46 +0530

    new commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 ## End to End Data Science Project
+
+MLFLOW_TRACKING_URI=https://dagshub.com/kanishkasah20/mlproject.mlflow \
+MLFLOW_TRACKING_USERNAME=kanishkasah20 \
+MLFLOW_TRACKING_PASSWORD=04fe4eb08dc627bb21b1b4ee28ec73a8b680547b \
+python script.py 
--- src/mlproject/components/model_trainer.py
+++ src/mlproject/components/model_trainer.py
@@ -2,8 +2,8 @@ import os
 import sys
 from dataclasses import dataclass
 from urllib.parse import urlparse
-# import mlflow
-# import mlflow.sklearn
+import mlflow
+import mlflow.sklearn
 import numpy as np
 from sklearn.metrics import mean_squared_error,mean_absolute_error
 from catboost import CatBoostRegressor
@@ -104,49 +104,50 @@ class ModelTrainer:
             ]
             best_model = models[best_model_name]
 
+# for mlflow and dagshub--start
             print("This is the best model:")
             print(best_model_name)
 
-            # model_names = list(params.keys())
+            model_names = list(params.keys())
 
-            # actual_model=""
+            actual_model=""
 
-            # for model in model_names:
-            #     if best_model_name == model:
-            #         actual_model = actual_model + model
+            for model in model_names:
+                if best_model_name == model:
+                    actual_model = actual_model + model
 
-            # best_params = params[actual_model]
+            best_params = params[actual_model]
 
-            # mlflow.set_registry_uri("https://dagshub.com/krishnaik06/mlprojecthindi.mlflow")
-            # tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+            mlflow.set_registry_uri("https://dagshub.com/kanishkasah20/mlproject.mlflow")
+            tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
-            # # mlflow
+            # mlflow - for tracking all the experiments
 
-            # with mlflow.start_run():
+            with mlflow.start_run():
 
-            #     predicted_qualities = best_model.predict(X_test)
+                predicted_qualities = best_model.predict(X_test)
 
-            #     (rmse, mae, r2) = self.eval_metrics(y_test, predicted_qualities)
+                (rmse, mae, r2) = self.eval_metrics(y_test, predicted_qualities)
 
-            #     mlflow.log_params(best_params)
+                mlflow.log_params(best_params)
 
-            #     mlflow.log_metric("rmse", rmse)
-            #     mlflow.log_metric("r2", r2)
-            #     mlflow.log_metric("mae", mae)
-
-
-            #     # Model registry does not work with file store
-            #     if tracking_url_type_store != "file":
-
-            #         # Register the model
-            #         # There are other ways to use the Model Registry, which depends on the use case,
-            #         # please refer to the doc for more information:
-            #         # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-            #         mlflow.sklearn.log_model(best_model, "model", registered_model_name=actual_model)
-            #     else:
-            #         mlflow.sklearn.log_model(best_model, "model")
+                mlflow.log_metric("rmse", rmse)
+                mlflow.log_metric("r2", r2)
+                mlflow.log_metric("mae", mae)
 
 
+                # Model registry does not work with file store
+                if tracking_url_type_store != "file":
+
+                    # Register the model
+                    # There are other ways to use the Model Registry, which depends on the use case,
+                    # please refer to the doc for more information:
+                    # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+                    mlflow.sklearn.log_model(best_model, "model", registered_model_name=actual_model)
+                else:
+                    mlflow.sklearn.log_model(best_model, "model")
+
+#  for mlflow and dagshub---end
 
 
             if best_model_score<0.6:
